4375c58b795c8365e4e4e049f25e8a6f8146c743
tashfeenrao <tashfeendev@gmail.com>
2022-10-18 19:32:31 +0500

    nested modals added

--- request_body/request_body.py
+++ request_body/request_body.py
@@ -1,19 +1,38 @@
 import uvicorn
 from fastapi import FastAPI, Path, Body, Query
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class Image(BaseModel):
+    # we can also some special extra validation for url too
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
-    name: str
+    # we can use Field() from pydantic to add extra validation
+    name: str = Field()
     description: str = None
     price: int
     tax: int = None
+    # we can also use list or dict type fastapi will make sure that request body include those types
+    tags: list[str]
+    image: Image
+    # we can also use pydantic modal as pure lists type
+    multiple_img: list[Image]
 
 
 class User(BaseModel):
     name: str
     age: int
     post: str
+
+
+class Offer(BaseModel):
+    # we can use deeply nested modal as type, fast api will give
+    # us all the conversion, validation, json conversion automatically
+    item: Item
+    date: str
 
 
 app = FastAPI()
@@ -54,23 +73,24 @@ def all_params(name: str, item: Item, q: int = None):
 # request body using the Body(embed=True)
 @app.post("/user/{user_id}/item/{item_id}")
 def multiple_user_body_params(
-    *,
-    user_id: int = Path(),
-    item_id: int = Path(),
-    item: Item = Body(),
-    user: User = Body(),
-    q: str | None = Query(default=None),
-    approval: bool = Body(default=False, embed=True)
+        *,
+        user_id: int = Path(),
+        item_id: int = Path(),
+        item: Item = Body(),
+        user: User = Body(),
+        q: str | None = Query(default=None),
+        approval: bool = Body(default=False, embed=True)
 ):
-    return {user_id, item_id, item, user}
+    item_dict = item.dict()
+    return {"u": user_id, "result": item_dict, "u": user}
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "request_body:app",
-        host="localhost",
-        port=8000,
-        reload=True,
-        debug=True,
-        workers=3,
+            "request_body:app",
+            host="localhost",
+            port=8000,
+            reload=True,
+            debug=True,
+            workers=3,
     )
